87f0f33c150989ebdaa963fe937aeb3dd4b508f4
TheKillerWallaby <dufresnec@gmail.com>
2023-01-05 18:12:18 -0500

    Added functionality that changes the blocks background with a mouseover

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 /* script.js */
 const container = document.querySelector('#divcontainer');
 
-
-function addElement() {
-  for (let i = 0; i <=255; i++) 
-    {const newDiv = document.createElement('cell');
-     newDiv.textContent= i;
-    divcontainer.appendChild(newDiv)
-   
-}}
-addElement();
-
-const newCell = document.querySelector('cell')
-
-newCell.addEventListener('mouseover', hover()); 
-
-function hover(){
-  newCell.setAttribute('style', 'background-color:black;')
+for (let i = 0; i <=255; i++) {
+    const newDiv = document.createElement('cell');
+    container.appendChild(newDiv)
+    newDiv.addEventListener('mouseover', () => {
+    newDiv.style.backgroundColor = "black"
+    }); 
 }
+
+
+
+
+
