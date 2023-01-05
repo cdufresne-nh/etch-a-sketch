95af3b3e7c008e4a3a367a372b187668055e4a66
TheKillerWallaby <dufresnec@gmail.com>
2023-01-05 17:16:55 -0500

    Created grid with JS

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 /* script.js */
+const container = document.querySelector('#divcontainer');
+
+
+function addElement() {
+  for (let i = 0; i <=255; i++) 
+    {const newDiv = document.createElement('cell');
+     newDiv.textContent= i;
+    divcontainer.appendChild(newDiv)
+   
+}}
+addElement();
+
+const newCell = document.querySelector('cell')
+
+newCell.addEventListener('mouseover', hover()); 
+
+function hover(){
+  newCell.setAttribute('style', 'background-color:black;')
+}
