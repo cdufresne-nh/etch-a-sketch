e5b19e70e181be310217eb1e11e3d47488da00fd
TheKillerWallaby <dufresnec@gmail.com>
2023-01-10 18:27:29 -0500

    Created button with prompt to enter in variable length

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 /* script.js */
 const container = document.querySelector('#divcontainer');
+const button = document.querySelector("#gridButton");
+let gridSize = 0;
 
-for (let i = 0; i <=255; i++) {
+function gridSizePopup() {
+	let gridLengthStr = prompt("Enter the number how long you want your grid","");
+	let gridLength = Number(gridLengthStr);
+	let gridSize = gridLength**2
+	return(gridSize);
+}
+
+for (let i = 0; i < gridSize; i++) {
     const newDiv = document.createElement('cell');
     container.appendChild(newDiv)
     newDiv.addEventListener('mouseover', () => {
-    newDiv.style.backgroundColor = "black"
-    })
+    newDiv.style.backgroundColor = "black";
+     })
 }
-
 
 
 
