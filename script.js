5abed8436951674f7bb0e37740cc78024d396608
TheKillerWallaby <dufresnec@gmail.com>
2023-03-22 13:42:58 -0400

    Created reset functionality

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 /* script.js */
-const container = document.getElementById('divcontainer');
+const container = document.getElementById('container');
 const button = document.querySelector("#gridButton");
 
-let gridSize = 256;
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for ( i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = "black";
+             })
+    };
+};
 
-if (gridSize == 0) {
+makeRows(16, 16);
+
+/* if (gridSize == 0) {
     gridSizePopup();
 } else {
     newGrid(gridSize);
-}
+} */
 
-function createGrid() {
-    const newDiv = document.createElement('cell');
-    container.appendChild(newDiv);
-    newDiv.addEventListener('mouseover', () => {
-    newDiv.style.backgroundColor = "black";
-     })
-    return newDiv;
-}
-
-function deleteGrid() {
-    container.remove(container);
-}
-
-function newGrid(gridSize) {
-    for (let i = 0; i < gridSize; i++) {
-        createGrid();
-    }
+function resetGrid() {
+    let allTiles = document.querySelectorAll(".grid-item");
+    allTiles.forEach((element) => 
+    element.style.backgroundColor = "white");
 }
 
 
@@ -35,4 +34,4 @@ function gridSizePopup() {
 	let gridLength = Number(gridLengthStr);
 	let gridSize = gridLength**2
 	return(gridSize);
-}
+} 
