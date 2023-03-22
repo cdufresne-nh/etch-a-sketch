bd8d74bc2c31ee9fb9461d3858afcc34a2b182c1
TheKillerWallaby <dufresnec@gmail.com>
2023-03-22 14:13:22 -0400

    Trying to troubleshoot left over divs

--- script.js
+++ script.js
@@ -16,22 +16,21 @@ function makeRows(rows, cols) {
 
 makeRows(16, 16);
 
-/* if (gridSize == 0) {
-    gridSizePopup();
-} else {
-    newGrid(gridSize);
-} */
-
 function resetGrid() {
     let allTiles = document.querySelectorAll(".grid-item");
-    allTiles.forEach((element) => 
-    element.style.backgroundColor = "white");
+    allTiles.forEach((element) => element.style.backgroundColor = "white");
 }
 
+function deleteGrid() {
+    container.removeChild.classname = "grid-item";
+}
 
 function gridSizePopup() {
-	let gridLengthStr = prompt("Enter the number how long you want your grid","");
+    deleteGrid();
+    resetGrid();
+	let gridLengthStr = prompt("Enter the number of how long you want your grid to be","");
 	let gridLength = Number(gridLengthStr);
-	let gridSize = gridLength**2
-	return(gridSize);
+	let gridSize = gridLength**2;
+    makeRows(gridLength, gridLength);
+	
 } 
